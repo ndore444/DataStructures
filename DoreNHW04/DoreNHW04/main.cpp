f599c8870fc7d89cb2c55382c769fc416c20d220
Nate Dore <ndore444@yahoo.com>
2018-02-17 21:13:21 -0500

    first commit

---
+++ DoreNHW04/DoreNHW04/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ *Name: Nathaniel Dore
+ *Class: CSCI 2421
+ *Description: [fill in description]
+ *Lab Partner: N/A
+ *Status: development complete: final release build 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+
+
+    system("Pause");
+    return 0;
+}
