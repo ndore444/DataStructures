28eec3eb8111c04bb36c9ad90234e223eb31d4e3
Nate Dore <ndore444@yahoo.com>
2018-03-05 10:42:59 -0500

    1038
    initial commit

---
+++ DoreNHW5/DoreNHW5/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ *Name: Nathaniel Dore
+ *Class: CSCI 2421
+ *Lab Partner: N/A
+ *Status: development complete: final release build 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+
+
+	/*
+	Remember to
+	rename the files! :)
+	change the date in the readme
+	change the class # in main.cpp and the readme
+	change the make file objects and exe name
+	*/
+
+    system("Pause");
+    return 0;
+}
